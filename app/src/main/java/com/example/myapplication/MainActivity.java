709936eb0fe86011acc82c65d2b0e0d9f7e579c8
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2020-05-28 14:41:10 +0200

    before testing font example

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,7 +1,7 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.widget.Button;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    Button myButton = (Button) findViewById(R.id.button);
+    Integer color = getResources().getColor(R.color.red);
+    myButton.setBackgroundColor(color);
   }
 }
--- app/src/main/java/com/example/myapplication/PieChart.java
+++ app/src/main/java/com/example/myapplication/PieChart.java
@@ -5,20 +5,14 @@ import android.view.ViewGroup.LayoutParams;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.View;
+import android.util.Log;
 
 public class PieChart extends View {
+  private static final String TAG = "PieChart";
   public PieChart(Context context, AttributeSet attrs) {
     super(context, attrs);
-    setLayoutParams(new LayoutParams(100,100));
-    // TypedArray a = context.getTheme().obtainStyledAttributes(
-    //   attrs,
-    //   R.styleable.PieChart,
-    //   0, 0);
-    // try {
-    //   mShowText = a.getBoolean(R.styleable.PieChart_showText, false);
-    //   textPos = a.getInteger(R.styleable.PieChart_labelPosition, 0);
-    // } finally {
-    //   a.recycle();
-    // }
+    TypedArray ta = context.obtainStyledAttributes(attrs, R.styleable.PieChart, 0, 0);
+    Boolean showText = ta.getBoolean(R.styleable.PieChart_showText, false);  
+    Log.w(TAG, "ta: " + showText);
   }
 }
