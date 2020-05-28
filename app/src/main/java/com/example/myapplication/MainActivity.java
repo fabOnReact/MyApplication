dea3098e3f30f7aa109282f3339cf182ca2e1b6a
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2020-05-28 14:45:59 +0200

    test case TExtView

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -5,14 +5,9 @@ import android.widget.Button;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-  private PieChart pieChart;
-
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
-    Button myButton = (Button) findViewById(R.id.button);
-    Integer color = getResources().getColor(R.color.red);
-    myButton.setBackgroundColor(color);
   }
 }
