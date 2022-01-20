d371cdde7a0d3fa0586d5788725e760e39389118
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2022-01-20 12:00:28 +0800

    counting ScrollView visible childrens

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,5 +1,11 @@
 package com.example.myapplication;
 
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.Rect;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
 import androidx.appcompat.app.AppCompatActivity;
 import android.widget.Button;
 import android.os.Bundle;
@@ -9,5 +15,26 @@ public class MainActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    ViewGroup linearLayout = findViewById(R.id.linear_layout);
+    linearLayout.setBackgroundColor(Color.RED);
+    getWindow().getDecorView().post(new Runnable() {
+        @Override
+        public void run() {
+          // TODO your magic code to be run
+          ViewGroup linearLayout = findViewById(R.id.linear_layout);
+          int height = linearLayout.getHeight()     ;
+          int width = linearLayout.getWidth();
+          Log.w("TESTING::", "height: " + height);
+          Rect scrollBounds = new Rect();
+          linearLayout.getHitRect(scrollBounds);
+          Log.w("TESTING::", "scrollBound: " + scrollBounds);
+          final int childCount = linearLayout.getChildCount();
+          for (int i = 0; i < childCount; i++) {
+            final View child = linearLayout.getChildAt(i);
+            boolean isVisible = child.getLocalVisibleRect(scrollBounds);
+            Log.w("TESTING::", "child i: " + i + " isVisible: " + isVisible);
+          }
+        }
+    });
   }
 }
