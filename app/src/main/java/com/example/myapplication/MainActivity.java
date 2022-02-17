b0950f8b9e649ea6a2222eb80ccca6ea4032379d
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2022-02-17 10:27:53 +0800

    init commit

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,40 +1,50 @@
 package com.example.myapplication;
 
-import android.content.Context;
-import android.graphics.Color;
-import android.graphics.Rect;
-import android.util.Log;
-import android.view.View;
-import android.view.ViewGroup;
-import androidx.appcompat.app.AppCompatActivity;
-import android.widget.Button;
 import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+  GridView gridView;
+  static final String[] numbers = new String[] {
+      "1", "2", "3", "4", "5",
+      "6", "7", "8", "9", "10",};
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
-    ViewGroup linearLayout = findViewById(R.id.linear_layout);
-    linearLayout.setBackgroundColor(Color.RED);
-    getWindow().getDecorView().post(new Runnable() {
-        @Override
-        public void run() {
-          // TODO your magic code to be run
-          ViewGroup linearLayout = findViewById(R.id.linear_layout);
-          int height = linearLayout.getHeight()     ;
-          int width = linearLayout.getWidth();
-          Log.w("TESTING::", "height: " + height);
-          Rect scrollBounds = new Rect();
-          linearLayout.getHitRect(scrollBounds);
-          Log.w("TESTING::", "scrollBound: " + scrollBounds);
-          final int childCount = linearLayout.getChildCount();
-          for (int i = 0; i < childCount; i++) {
-            final View child = linearLayout.getChildAt(i);
-            boolean isVisible = child.getLocalVisibleRect(scrollBounds);
-            Log.w("TESTING::", "child i: " + i + " isVisible: " + isVisible);
-          }
-        }
+
+    gridView = (GridView) findViewById(R.id.gridView1);
+
+    ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+        android.R.layout.simple_list_item_1, numbers);
+
+    gridView.setAdapter(adapter);
+
+    gridView.setOnItemClickListener(new OnItemClickListener() {
+
+      @Override
+      public void onItemClick(AdapterView<?> parent, View view, int position,long id) {
+        Toast.makeText(getApplicationContext(),((TextView) view).getText(), Toast.LENGTH_LONG).show();
+
+      }
+
+
     });
   }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    // Inflate the menu; this adds items to the action bar if it is present.
+    getMenuInflater().inflate(R.menu.activity_main, menu);
+    return true;
+  }
+
 }
